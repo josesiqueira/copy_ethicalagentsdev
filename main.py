9fa044ccb16d754a2c7826be85737860ef3bf679
José Siqueira <47609042+josesiqueira@users.noreply.github.com>
2025-05-12 14:24:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,10 @@ st.set_page_config(page_title="Agents4EthicalSE")
 load_dotenv()
 
 # Set OpenAI API key and model
-api_key = st.secrets["OPENAI_API_KEY"]  # or load however you'd like
-api_client = OpenAI(api_key=api_key)
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+api_client = OpenAI()
+#api_key = st.secrets["OPENAI_API_KEY"]  # or load however you'd like
+#api_client = OpenAI(api_key=api_key)
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 #api_client = openai.OpenAI(api_key=openai.api_key)
 model = "gpt-4o-mini"
